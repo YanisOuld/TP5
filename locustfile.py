35bec9b08fa4bf5d3b7b07cac13f6eca332d9b3e
YanisOuld <yanis.ould-mahammed@polymtl.ca>
2025-03-25 14:31:55 -0400

    feature: locust in pipeline

--- locustfile.py
+++ locustfile.py
@@ -27,6 +27,41 @@ class SimpleTasks(TaskSet):
         payload = {"message": "Hello, Locust!"}
         self.client.post("/echo", json=payload)
 
+class HeavyTasks(TaskSet):
+    @task(1)
+    def home(self):
+        self.client.get("/")
+        
+    @task(2)
+    def fast(self):
+        self.client.get("/fast")
+        
+    @task(3)
+    def medium(self):
+        self.client.get("/medium")
+        
+    @task(4)
+    def heavy(self):
+        self.client.get("/heavy")
+        
+    @task(1)
+    def echo(self):
+        payload = {"message": "Hello, Locust!"}
+        self.client.post("/echo", json=payload)
+
 class SimpleUser(HttpUser):
     tasks = [SimpleTasks]
     wait_time = between(1, 5)  # Simulate a wait time between requests
+    weight = 1
+
+    def on_start(self):
+        print("SimpleUser started")
+
+class HeavyUser(HttpUser):
+    tasks = [HeavyTasks]
+    wait_time = between(5, 10)
+    weight = 1
+
+    def on_start(self):
+        print("HeavyUser started")
+
